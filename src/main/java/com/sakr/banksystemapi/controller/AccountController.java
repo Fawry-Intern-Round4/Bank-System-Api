264a71185b51c02b8dcf34ed6317336e788d4939
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-10-06 17:44:38 +0300

    start with unit teasting

--- src/main/java/com/sakr/banksystemapi/controller/AccountController.java
+++ src/main/java/com/sakr/banksystemapi/controller/AccountController.java
@@ -6,7 +6,6 @@ import com.sakr.banksystemapi.model.account.AccountTransactionHistoryModel;
 import com.sakr.banksystemapi.service.AccountService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
